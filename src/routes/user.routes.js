58815b18a2bc28bbd64428a993fccf5ed43889a9
Ayush Maurya <amaurya5556@gmail.com>
2024-01-13 18:17:46 +0530

    Add user controller routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -34,5 +34,15 @@ router.route("/login").post(loginUser);
 // Secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
