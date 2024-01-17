9e8e5e73b3830ba6796069a5e5aabc9aefb5cd92
Ayush Maurya <amaurya5556@gmail.com>
2024-01-17 23:15:29 +0530

    Add Like model

---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
